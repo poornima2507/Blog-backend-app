fa67eb14feb7a8feaa7cabe54fc75d93014525a7
poornima2507 <poornimaofficial25@gmail.com>
2024-06-13 11:25:26 +0530

    created

---
+++ models/blog.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+const schema =  mongoose.Schema(
+    {
+        "name":String,
+        "emailid":String,
+        "password":String
+        
+    }
+)
+let blogmodel = mongoose.model("users",schema);
+module.exports = {blogmodel}

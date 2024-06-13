31929bbbc86ddd99494ab307812fb5896275c4e3
poornima2507 <poornimaofficial25@gmail.com>
2024-06-13 15:21:28 +0530

    created

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const bcrypt = require("bcryptjs")
 const {blogmodel} = require("./models/blog")
+const jwt = require("jsonwebtoken")
 
 mongoose.connect("mongodb+srv://poornima25:poornima25182220@cluster0.dg8g8.mongodb.net/blogdb?retryWrites=true&w=majority&appName=Cluster0")
 const app = express()
@@ -23,6 +24,39 @@ app.post("/signup",async(req,res)=>{
     let blog = new blogmodel(input)
     blog.save()
     res.json({"status":"success"})
+})
+
+app.post("/signin",(req,res)=>{
+    let input = req.body
+    blogmodel.find({"emailid":req.body.emailid}).then(
+        (response)=>{
+            if (response.length>0){
+                let dbPassword = response[0].password
+                console.log(dbPassword)
+                bcrypt.compare(input.password,dbPassword,(error,isMatch)=>{
+                    if (isMatch){
+                        jwt.sign({email:input.emailid},"blog-app",{expiresIn:"1d"},
+                    (error,token)=>{
+                        if (error) {
+                            res.json({"status":"unable to create token"})
+                        }
+                        else {
+                            res.json({"status":"success","userId":response[0]._id,"token":token})
+                        }
+                    })
+                    
+                    }
+                    else {
+                        res.json({"status":"incorrect"})
+                    }
+                })
+
+            }
+            else{
+                res.json({"status":"user not found"})
+            }
+        }
+    ).catch()
 })
 
 app.listen(8085,()=>{
